78f0f39b526c8c7bef925554578e17e9d93dbc63
angel <angelsb94@gmail.com>
2020-02-22 20:43:09 -0600

    rediseno del logo de la pagina de la barra de navegacion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,16 +11,18 @@ function NavBar() {
         
         <nav>
             <div className="row  cyan darken-4">
-                <div className="col s2">
+                <div className="col s3">
+                    <a href="https://www.themoviedb.org/" target='_blank'>
                     <img src={configServices.imgLogMovie} 
                     alt="Logo peli"
                     className='valign-wrapper' 
-                    id='LogNav'/>                    
+                    id='LogNav'/>
+                    </a>                
                 </div>
                 <div className="col s2 pull-s1">
                     <h4 className='valign-wrapper'>Movie-React</h4>
                 </div>
-                <div className="col s1 offset-s6">
+                <div className="col s1 offset-s5">
                     <ul id="nav-mobile" className="left hide-on-med-and-down">
                         <li><Link to="/"><h5 className='valign-wrapper'>Home</h5></Link></li>
                     </ul>
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,8 +2,7 @@ const configServices={
     apiUrl : 'https://api.themoviedb.org/3/',
     apiKey : '0d59c137d4b1775154cc094577fbe290',
     ImgBaseUrl: 'http://image.tmdb.org/t/p/',
-    imgLogMovie:`https://images.vexels.com/media/users/3/153138/isolated/preview/136988c6187d1768a88ef4918e2bd2f9-icono-plano-de-cinta-de-pel--cula-by-vexels.png`
-
+    imgLogMovie:'https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png'
 }
 
 
